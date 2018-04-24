2dc3e1386100de6cd56fcb14f6a0aeb538fa2a17
Max Hully <max.hully@gmail.com>
2018-04-23 22:04:44 -0400

    Got the Mapbox map to paint the census tracts! I had the geoid code wrong. 140, not 160!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
-import React, { Component } from "react";
+import React from "react";
+import Map from "./components/Map";
+
+/*import MapGL from "react-map-gl";
+import DeckGL, { GeoJsonLayer } from "deck.gl";
+
+import tracts from "./data/wisconsin_tracts.json";
+
+const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewport: {
+                width: 1080,
+                height: 720,
+                longitude: -92.0,
+                latitude: 45.0,
+                zoom: 5,
+                pitch: 0,
+                bearing: 0
+            }
+        };
+    }
+    _onViewportChange = viewport => {
+        this.setState(state => ({
+            ...this.state,
+            viewport: { ...state.viewport, ...viewport }
+        }));
+    };
     render() {
+        const tractsLayer = new GeoJsonLayer({
+            id: "tracts",
+            data: tracts,
+            filled: true,
+            stroked: false,
+            extruded: false,
+            getElevation: f => f.properties["2013_population_estimate"]
+        });
+        const { viewport } = this.state;
         return (
             <main>
+                <MapGL
+                    {...viewport}
+                    onViewportChange={this._onViewportChange}
+                    mapboxApiAccessToken={MAPBOX_TOKEN}
+                >
+                    <DeckGL {...viewport} layers={[tractsLayer]} />
+                </MapGL>
             </main>
         );
     }
+}*/
+
+function App(props) {
+    return <Map />
 }
 
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import mapboxgl from "mapbox-gl";
 
+//const counties =
+//    "https://api.censusreporter.org/1.0/geo/show/tiger2016?geo_ids=050|04000US55";
+const tracts = "https://api.censusreporter.org/1.0/geo/show/tiger2016?geo_ids=140|04000US55"
+
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 const DEFAULT_OPTIONS = {
     style: "mapbox://styles/mapbox/streets-v9",
-    center: [-71.0589, 42.3601],
-    zoom: 9
+    center: [-90.0, 44.7],
+    zoom: 6
 };
 
 export default class Map extends Component {
@@ -14,6 +18,36 @@ export default class Map extends Component {
         this.map = new mapboxgl.Map({
             ...DEFAULT_OPTIONS,
             container: "map"
+        });
+
+        this.map.on("load", () => {
+            this.map.addSource("tracts", { type: "geojson", data: tracts });
+            this.map.addLayer(
+                {
+                    id: "tracts",
+                    type: "fill",
+                    source: "tracts",
+                    layout: {},
+                    paint: {
+                        "fill-color": "#FF7F50",
+                        "fill-opacity": 0.8
+                    }
+                },
+                "water"
+            );
+            this.map.addLayer(
+                {
+                    id: "tracts-outline",
+                    type: "line",
+                    source: "tracts",
+                    layout: {},
+                    paint: {
+                        "line-color": "#AAAAAA",
+                        "line-opacity": 0.8
+                    }
+                },
+                "water"
+            );
         });
     }
     render() {
